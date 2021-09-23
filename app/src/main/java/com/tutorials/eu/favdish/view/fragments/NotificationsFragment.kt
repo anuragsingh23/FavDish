dca25fdbdeb314d44efca75b2191c79e315857c7
anuragsingh23 <anuragshekhawat17@gmail.com>
2021-09-23 21:10:58 +0530

    mvvm setup and adding fragments and bottom navigation as well as menu

--- app/src/main/java/com/tutorials/eu/favdish/view/fragments/NotificationsFragment.kt
+++ app/src/main/java/com/tutorials/eu/favdish/view/fragments/NotificationsFragment.kt
@@ -1,4 +1,4 @@
-package com.tutorials.eu.favdish.ui.notifications
+package com.tutorials.eu.favdish.view.fragments
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -9,6 +9,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.tutorials.eu.favdish.R
+import com.tutorials.eu.favdish.viewmodel.NotificationsViewModel
 
 class NotificationsFragment : Fragment() {
 
