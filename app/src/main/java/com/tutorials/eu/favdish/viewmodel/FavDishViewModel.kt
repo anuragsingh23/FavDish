717147b2424750118971e10557682b75beb8f420
anuragsingh23 <anuragshekhawat17@gmail.com>
2021-10-18 21:12:20 +0530

    Setting up ViewModel as well as application class

--- app/src/main/java/com/tutorials/eu/favdish/viewmodel/FavDishViewModel.kt
+++ app/src/main/java/com/tutorials/eu/favdish/viewmodel/FavDishViewModel.kt
@@ -1,7 +1,32 @@
 package com.tutorials.eu.favdish.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
 import com.tutorials.eu.favdish.model.database.FavDishRepository
+import com.tutorials.eu.favdish.model.entities.FavDish
+import kotlinx.coroutines.launch
 
 class FavDishViewModel(private val repository: FavDishRepository):ViewModel() {
+
+
+    fun insert(dish : FavDish)= viewModelScope.launch {
+        repository.insertFavDishData(dish)
+
+    }
+
+
+}
+
+class FavDishViewModelFactory(private val repository: FavDishRepository) :ViewModelProvider.Factory{
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(FavDishViewModel::class.java)){
+
+            @Suppress("UNCHECKED_CAST")
+            return FavDishViewModel(repository) as T
+        }
+        throw IllegalArgumentException(
+                "unkwon viewmodel class")
+    }
+
 }
