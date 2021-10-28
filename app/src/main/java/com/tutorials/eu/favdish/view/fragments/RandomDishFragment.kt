9db744d0fbc6313f9c97cd008266e86916995b38
anuragsingh23 <anuragshekhawat17@gmail.com>
2021-10-28 10:07:00 +0530

    begin of retrofit2 in RandomDishFragment.kt

--- app/src/main/java/com/tutorials/eu/favdish/view/fragments/RandomDishFragment.kt
+++ app/src/main/java/com/tutorials/eu/favdish/view/fragments/RandomDishFragment.kt
@@ -9,10 +9,12 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.tutorials.eu.favdish.R
+import com.tutorials.eu.favdish.databinding.FragmentRandomDishBinding
 import com.tutorials.eu.favdish.viewmodel.NotificationsViewModel
 
 class RandomDishFragment : Fragment() {
 
+    private var mBinding:FragmentRandomDishBinding? = null
     private lateinit var notificationsViewModel: NotificationsViewModel
 
     override fun onCreateView(
@@ -20,13 +22,15 @@ class RandomDishFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        notificationsViewModel =
-            ViewModelProvider(this).get(NotificationsViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_random_dish, container, false)
-        val textView: TextView = root.findViewById(R.id.text_notifications)
-        notificationsViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
+      mBinding= FragmentRandomDishBinding.inflate(inflater,container,false)
+
+
+
+        return mBinding!!.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mBinding = null
     }
 }
